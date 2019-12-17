c3c99a9aa05cfd39aed7e6ab9340c08337cbc5c1
Klaus Jonke <klaus.jonke@at.zurich.com>
2019-12-17 16:31:47 +0100

    * fixed DateTimeTests by using regular expression and re-parsing Iso-timestamp

--- Camunda.Api.Client.Tests/DateTimeTests.cs
+++ Camunda.Api.Client.Tests/DateTimeTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Camunda.Api.Client.Extensions;
 using Xunit;
 
@@ -8,16 +9,16 @@ namespace Camunda.Api.Client.Tests
     public class DateTimeTests
     {
         [Theory]
-        [InlineData("2010-01-01T01:01:01.000", "2010-01-01T01:01:01.000+0100")]
-        [InlineData("2010-05-01T01:01:01.000", "2010-05-01T01:01:01.000+0200")]
-        public void GetDateTime(string testDateTimeString,string expectedDatetimeString)
+        [InlineData("2010-01-01T01:01:01.000")]
+        [InlineData("2010-05-01T01:01:01.000")]
+        public void GetDateTime(string testDateTimeString)
         {
             var date = DateTime.Parse(testDateTimeString);
             var actualIso8601DateString = date.ToJavaISO8601();
             var resultDate = DateTime.Parse(actualIso8601DateString);
             Assert.Equal(date, resultDate);
-            //compare actualIso8601DateString with regexp instead of this:
-            //Assert.Equal(expectedDatetimeString, actualIso8601DateString);
+            var regEx = new Regex(@"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}[+-]\d{4}$");
+            Assert.Matches(regEx, actualIso8601DateString);
         }
     }
 }
